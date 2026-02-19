c1f755fa72776c4725297b4c2b5ed476e1b75bbe
Dusan Navidu <navidu200210@gmail.com>
2026-02-19 16:06:43 +0530

    back end initialize

---
+++ src/main/java/com/omnipos/omniposbackend/OmniposBackendApplication.java
@@ -0,0 +1,13 @@
+package com.omnipos.omniposbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniposBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniposBackendApplication.class, args);
+	}
+
+}
